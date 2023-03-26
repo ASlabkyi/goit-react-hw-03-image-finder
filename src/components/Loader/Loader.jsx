f6c89338d7be3c82b446d06ac008f9a6f5384df1
Andrii Slabkyi <aslabkyi@gmail.com>
2023-03-26 15:29:38 +0300

    add loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,9 +1,9 @@
-// import { RotatingLines } from 'react-loader-spinner';
+import { RotatingLines } from 'react-loader-spinner';
 
-// <RotatingLines
-//   strokeColor="grey"
-//   strokeWidth="5"
-//   animationDuration="0.75"
-//   width="96"
-//   visible={true}
-// />;
+<RotatingLines
+  strokeColor="grey"
+  strokeWidth="5"
+  animationDuration="0.75"
+  width="96"
+  visible={true}
+/>;
